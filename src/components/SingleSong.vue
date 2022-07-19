5bdf6b308a96560136aaa75fcd70252720716b5b
bigeppo <carlog91cast@gmail.com>
2022-07-19 17:57:19 +0200

    add singleson

---
+++ src/components/SingleSong.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card">
+    <div class="song-img">
+        <img :src="info.poster" alt="">
+    </div>
+    <h3>{{info.title}}</h3>
+    <h4>{{info.author}}</h4>
+    <h5>{{info.year}}</h5>
+</div>
+</template>
+
+<script>
+export default {
+    name: "SongCard",
+    props: ['info'],
+}
+</script>
+
+<style lang="scss" scoped>
+.card{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 1rem;
+        background-color: $bg_first_color;
+        text-align: center;
+        margin-bottom: 1rem;
+        .song-img{
+            width: 100%;
+            aspect-ratio: 1/1;
+            background-color: black;
+        }
+        img{
+            width:100% ;
+        }
+        h3{
+            width: 100%;
+            font-size: 1.5rem;
+            padding: 0.5rem 0;
+            margin-bottom: 0;
+            color: white;
+        }
+        h4{
+            width: 100%;
+            font-size: 1.2rem;
+            color: $subtext_color;
+            margin-bottom: 0;
+        }
+        h5{
+            width: 100%;
+             font-size: 1rem;
+            color: $subtext_color;
+        }
+    }
+</style>
--- src/components/SongMain.vue
+++ src/components/SongMain.vue
@@ -1,10 +1,9 @@
 
 
 <template>
-    <div class="container">
-        <ListSong />
+    <div class="container song-container">
         <div class="row">
-            <div class="col-2">
+            <div v-for(song, index) in   class="col-2">
                 <SingleSong />
             </div>
         </div>
@@ -14,19 +13,21 @@
 <script>
 import axios from "axios";
 import SingleSong from './SingleSong.vue';
-import ListSong from './ListSong.vue';
+
 
 export default {
     name: "SongMain",
     components: {
         SingleSong,
-        ListSong,
     },
     data() {
-
+        return{
+            url: "https://flynn.boolean.careers/exercises/api/array/music",
+            songList:[],
+        }
     },
     created() {
-        this.getSongCard;
+        this.getSongCard();
     },
     methods: {
         getSongCard() {
@@ -46,4 +47,9 @@ export default {
 
 <style lang="scss" scoped>
 @import '../style/common.scss';
+
+.song-container{
+    background-color: #1E2D3B;
+    height: 100%;
+}
 </style>
