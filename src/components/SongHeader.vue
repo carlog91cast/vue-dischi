046844b5084a6f72fa76ae16ea4d2232d5ce35e8
bigeppo <carlog91cast@gmail.com>
2022-07-19 17:32:43 +0200

    add axios

--- src/components/SongHeader.vue
+++ src/components/SongHeader.vue
@@ -3,7 +3,7 @@
 <template>
     <header>
         <div class="spotify-logo">
-            <img src="../assets/Spotify_logo_without_text.svg.png" alt="spotify logo">
+            <img class="w-100" src="../assets/Spotify_logo_without_text.svg.png" alt="spotify logo">
         </div>
     </header>
 </template>
@@ -13,6 +13,22 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 @import '../style/common.scss';
+
+header {
+    width: 100vw;
+    height: 50px;
+    background-color: #2E3A46;
+    color: white;
+
+    .spotify-logo {
+        img {
+            height: 30px;
+            padding: 0 1.5rem;
+            margin-top: 10px;
+        }
+    }
+
+}
 </style>
--- src/components/SongMain.vue
+++ src/components/SongMain.vue
@@ -1,17 +1,49 @@
 
 
 <template>
-    <div>
-
+    <div class="container">
+        <ListSong />
+        <div class="row">
+            <div class="col-2">
+                <SingleSong />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+import SingleSong from './SingleSong.vue';
+import ListSong from './ListSong.vue';
+
 export default {
+    name: "SongMain",
+    components: {
+        SingleSong,
+        ListSong,
+    },
+    data() {
+
+    },
+    created() {
+
+    },
+    methods: {
+        getSongCard() {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((result) =>{
+                console.log(result.data.result)
+                this.songs = result.data.result;
+            })
+            .catch((error)=>{
+                console.warn(error);
+            })
+        }
+    }
 }
+
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 @import '../style/common.scss';
-
 </style>
